392af6f5533902acb40c475c1c33b0b06e8d2042
Simon Machado <donsimonmt@hotmail.com>
2017-03-27 12:00:10 -0400

    Update ClaseModulo.php

--- planificacion/modelo/ClaseModulo.php
+++ planificacion/modelo/ClaseModulo.php
@@ -8,13 +8,23 @@ class modulo{
    						 VALUES ('$cod_mod','$nom_mod','$cap_mod')";
 		$resultado=pg_query($conexion,$sql);
 		if (pg_affected_rows($resultado)>0) return 1; 
-		else return 0;		  	
-		
+		else return 0;		
 	}
+
+	function buscaModulo($cod_mod){		
+		include_once('../conexion.php');
+		$Consulta = "select * from modulo where codigo_modulo='$cod_mod'";
+		$resultado=pg_query($conexion,$Consulta);	
+		$Datos=@pg_fetch_all($resultado);//Devuelve los datos en forma de arreglo
+		if($Datos[0]['codigo_modulo'])//verificar si arrojo algun resultado
+			return $Datos;
+		else
+			return 0;
+	}	
 
 	function BuscaMod(){
 		include_once("../conexion.php");
-		if ($_SESSION['cod_dep']==10) {
+		if ($_SESSION['tipo_coordinador']==1) {
 			$sql="select * from modulo order by nombre_modulo";
 			$resultado=pg_query($conexion,$sql);
 			$esp=@pg_fetch_all($resultado);
