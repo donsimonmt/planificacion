10a7adfaa0b6f6a60b583c1604ec954aef0b58eb
Simon Machado <donsimonmt@hotmail.com>
2017-03-27 12:00:34 -0400

    Update claseSalon.php

--- planificacion/modelo/claseSalon.php
+++ planificacion/modelo/claseSalon.php
@@ -8,11 +8,19 @@ class SALON{
 	public function registrar($cod_salon,$nom_salon,$mod_salon,$cap_salon,$tipo_salon){
 		include_once ("../conexion.php");
 		$sql="insert into salon (codigo_salon,codigo_modulo,nombre_salon,capacidad_salon,tipo_salon)  
-					      values('$cod_salon','$mod_salon','$nom_salon','$cap_salon','$tipo_salon')";
-	
+					      values('$cod_salon','$mod_salon','$nom_salon','$cap_salon','$tipo_salon')";	
 		$resultado=pg_query($conexion,$sql);	
 		if(pg_affected_rows($resultado)>0) return 1;
 		else return 0; 
+	}
+
+	function BuscaSalon($cod_salon){
+		include_once("../conexion.php");
+		$sql="SELECT * FROM salon WHERE codigo_salon='$cod_salon'";		
+		$respuesta=pg_query($conexion,$sql);
+		$esp=@pg_fetch_all($respuesta);
+		  if ($esp[0]['codigo_salon']) return $esp;
+		  else return 0;
 	}
 }
 
