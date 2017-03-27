06f7096b41140c3d061e1c55459c0ec659e8af5c
Simon Machado <donsimonmt@hotmail.com>
2017-03-27 11:58:55 -0400

    Update modulo.js

--- planificacion/js/modulo.js
+++ planificacion/js/modulo.js
@@ -1,38 +1,56 @@
-function RegistraMod(){
-	
+function RegistraMod(){	
 	AjaxRequest.get
-	(
-		{
-
-			'parameters':{
+	({
+		'parameters':{
 				'cod_mod':$('#cod_mod').val(),
 				'nom_mod':$('#nom_mod').val(),
 				'cap_mod':$('#cap_mod').val(),
 				'accion':'RegistrarMod'
-			}
-		
-		  ,'onSuccess':function(req){
-			if(req.responseText==1)
-    			{
-	 				alert("Datos Guardados satisfactoriamente."); 
-					
-	 					
-				}else alert("error en el registro");
-			}
-	      ,'url':'controlador/modulo.php'
-		  ,'onError':function(req){
-				alert('HA OCURRIDO UN ERROR INTERNO INESPERADO');
-			}
 		}
-	);
+		 ,'onSuccess':function(req){
+		if(req.responseText==1)
+    		{
+	 			alert("Datos Guardados satisfactoriamente."); 
+			}else alert("error en el registro");
+		}
+	     ,'url':'controlador/modulo.php'
+		 ,'onError':function(req){
+			alert('HA OCURRIDO UN ERROR INTERNO INESPERADO');
+		}
+	});
 }
 
 function CancelaMod(){
 
-	$('#cod_mod').val(''),
+	$('#cod_mod').val('').focus(),
 	$('#nom_mod').val(''),
 	$('#cap_mod').val(''),
-	$('#enviar').prop('desibled',true)
+	$('#enviar').prop('disabled',false),
+	$('#modificar').prop('disabled',true)
+}
+
+function buscaModulo(){
+	AjaxRequest.get({
+			'parameters':{
+				'cod_mod':$('#cod_mod').val(),
+				'accion':'buscaModulo'
+			}
+			,'onSuccess':function(req){
+				var resultado=req.responseText;
+				var dato=eval("("+resultado+")");
+   				if(resultado!=0){	
+   					$('#nom_mod').val(dato[0]['nombre_modulo']),
+					$('#cap_mod').val(dato[0]['cantidad_salon_modulo']),
+					$('#enviar').prop('disabled',true),
+					$('#modificar').prop('disabled',false)					
+										
+				}else alert("Usuario no registrado");
+			}
+			,'url':'controlador/modulo.php'
+			,'onError': function(req){
+					alert('HA OCURRIDO UN ERROR INTERNO INESPERADO!\nStatusText='+req.statusText+'\nContents='+req.responseText);
+			}	
+	});
 }
 
 function BuscaMod(){
