bf639c223db0ca6900ccb690f2dfd01a0879b907
Simon Machado <donsimonmt@hotmail.com>
2017-03-27 19:44:06 -0400

    Update index_admin.php

--- planificacion/index_admin.php
+++ planificacion/index_admin.php
@@ -32,6 +32,7 @@
     <link rel="stylesheet" href="css/style_admin.css">
     <link rel="stylesheet" href="css/iniciar_sesion.css">
     <link rel="stylesheet" href="css/estilos.css">
+    <link rel="icon" href="imagenes/logo-uptos.gif" />
 </head>
 <body>
 	<header class="navbar-fixed-top">
@@ -100,13 +101,61 @@
 	</header>
      	
 	<section id="contenedor" class="container"><br><br>
-	<h1>¡Bienvenido! <?php echo $_SESSION['nombre_usuario']."."; ?> </h1><br>
-		<dev id="banner">
-       <img src="imagenes/portada.jpg" alt="UPTOS Cumana" class="container">
-		</dev>
+	
+
+	<div id="bienvenido">
+		<h3> <span>¡Bienvenido!</span> <span><?php echo $_SESSION['nombre_usuario']?></span> <span><?php echo $_SESSION['apellido_usuario']?></span></h3>
+	</div>	<br>
+
+
+
+
+
+		<div id="banner" >
+				<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="imagenes/pnf1.jpg" alt="" class="container">
+    </div>
+
+    <div class="item">
+      <img src="imagenes/pnf2.jpg" alt="" class="container">
+    </div>
+
+    <div class="item">
+      <img src="imagenes/pnf3.jpg" alt="" class="container">
+    </div>
+
+    <div class="item">
+      <img src="imagenes/pnf4.jpg" alt="" class="container">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Siguiente</span>
+  </a>
+</div>
+       		</div>
+       		
+		</div>
 	</section>
 
-	<footer class="navbar navbar-default navbar-bottom navbar-fixed-bottom"> 
+	<footer class="navbar navbar-default navbar-bottom navbar-fixed-bottom" >
 		<p>Universidad Politécnica Territorial del Oeste de Sucre "Clodosbaldo Russián", Km.4 Carretera Cumaná - Cumanacoa 
 		Tlf(s): (0293) 4672138 / 4672136 / 4672150 / 4672154 Fax (0293) 4672153 - Rif: G-20010205-5 </p>
 	</footer>	
@@ -135,3 +184,36 @@
   }
 </script>
 
+<style>
+	
+#bienvenido h3 {
+  /* margin: 20px; */
+  font-family: "helvetica";
+  color: #202020;
+ 
+ 
+}
+#bienvenido{
+	background: #E9E9E9;
+	padding: 1em;
+	width: 80%;
+	margin:auto;
+}
+
+#bienvenido  h3 span {
+  display: block;
+  margin: 11px 0 17px 0;
+  font-size: 60px;
+  line-height: 60px;
+  
+  text-shadow: 0 13.36px 8.896px #BBBBBB,0 -2px 1px #fff;
+  letter-spacing: -4px;
+}
+
+#bienvenido  h3 span:nth-child(2){
+	
+	color: #686262;
+}
+
+
+</style>
